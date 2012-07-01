780e2fc1c63a0fd1f5cff9cbca0d56c386de166b
Eric Herman <eric@freesa.org>
2012-07-01 17:26:41 +0200

    convert to a running script
     enabled c++
     extract the wget commands into separate script with updated versions
     reference a SOURCES directory where the tarballs have placed
     extract a PREFIX variable, default to installing in subdir of home for user
     script will now halt if any command fails

---
+++ wget.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# wget ftp://ftp.gnu.org/gnu/binutils/binutils-2.19.tar.gz
+wget ftp://ftp.gnu.org/gnu/binutils/binutils-2.22.tar.bz2
+
+# wget ftp://ftp.gnu.org/gnu/gcc/gcc-4.3.2/gcc-core-4.3.2.tar.gz
+wget ftp://ftp.gnu.org/gnu/gcc/gcc-4.7.1/gcc-4.7.1.tar.bz2
+
+# wget http://savannah.inetbridge.net/avr-libc/avr-libc-1.6.4.tar.bz2
+wget http://download.savannah.gnu.org/releases/avr-libc/avr-libc-1.8.0.tar.bz2
+
+# wget http://nongnu.askapache.com/avrdude/avrdude-5.8.tar.gz
+wget http://download.savannah.gnu.org/releases/avrdude/avrdude-5.11.1.tar.gz
+
+# wget http://ftp.gnu.org/gnu/gdb/gdb-6.8.tar.gz
+wget ftp://ftp.gnu.org/gnu/gdb/gdb-7.4.1.tar.bz2
+
+wget http://download.savannah.gnu.org/releases/simulavr/simulavr-1.0.0.tar.gz
