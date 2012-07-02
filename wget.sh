0fe0b837110053781798f70fccfbde36141e26de
Eric Herman <eric@freesa.org>
2012-07-02 07:39:42 +0200

    improve documentation

--- wget.sh
+++ wget.sh
@@ -8,12 +8,9 @@ wget ftp://ftp.gnu.org/gnu/gcc/gcc-4.7.1/gcc-4.7.1.tar.bz2
 
 # wget http://savannah.inetbridge.net/avr-libc/avr-libc-1.6.4.tar.bz2
 wget http://download.savannah.gnu.org/releases/avr-libc/avr-libc-1.8.0.tar.bz2
-#   hardware/arduino/cores/arduino/HardwareSerial.cpp:91:41:
-#      error: attempt to use poisoned "SIG_USART0_RECV"
 # avr-ibc-1.8.0 seems to have depreciated some of the required features
-# by the Arduino libraries.
-# See:
-#    http://arduino.cc/forum/index.php?topic=92364.0
+# by the Arduino libraries. See:
+#    patches/arduino_workaround_avr-libc-1.8.0_issue.patch
 
 # wget http://nongnu.askapache.com/avrdude/avrdude-5.8.tar.gz
 wget http://download.savannah.gnu.org/releases/avrdude/avrdude-5.11.1.tar.gz
