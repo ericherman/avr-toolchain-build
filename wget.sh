01059effe7af1274a5f5d6da433bc594c44ece7e
Eric Herman <eric@freesa.org>
2016-05-13 19:16:33 +0200

    GCC 6.1.0, binutils 2.26, avr-libc 2.0.0, etc.
    gcc-5.2.0 -> gcc-6.1.0
    binutils-2.25.1 -> binutils-2.26
    gmp-4.3.2 -> gmp-6.1.0
    mpfr-2.4.2 -> mpfr-3.1.4
    mpc-0.8.1 -> mpc-1.0.3
    avr-libc-1.8.1 -> avr-libc-2.0.0
    avrdude-6.1 -> avrdude-6.3
    gdb-7.10 -> gdb-7.11

--- wget.sh
+++ wget.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-wget http://ftp.gnu.org/gnu/binutils/binutils-2.25.1.tar.bz2
+wget http://ftp.gnu.org/gnu/binutils/binutils-2.26.tar.bz2
 
-wget ftp://ftp.gnu.org/gnu/gcc/gcc-5.2.0/gcc-5.2.0.tar.bz2
-wget ftp://gcc.gnu.org/pub/gcc/infrastructure/gmp-4.3.2.tar.bz2
-wget ftp://gcc.gnu.org/pub/gcc/infrastructure/mpfr-2.4.2.tar.bz2
-wget ftp://gcc.gnu.org/pub/gcc/infrastructure/mpc-0.8.1.tar.gz
+wget ftp://ftp.gnu.org/gnu/gcc/gcc-6.1.0/gcc-6.1.0.tar.bz2
+wget ftp://gcc.gnu.org/pub/gcc/infrastructure/gmp-6.1.0.tar.bz2
+wget ftp://gcc.gnu.org/pub/gcc/infrastructure/mpfr-3.1.4.tar.bz2
+wget ftp://gcc.gnu.org/pub/gcc/infrastructure/mpc-1.0.3.tar.gz
 
-# wget http://download.savannah.gnu.org/releases/avr-libc/avr-libc-1.8.0.tar.bz2
-# avr-ibc-1.8.0 seems to have depreciated some of the required features
-# by the Arduino libraries. See:
-#    patches/arduino_workaround_avr-libc-1.8.0_issue.patch
-wget http://download.savannah.gnu.org/releases/avr-libc/avr-libc-1.8.1.tar.bz2
+wget http://download.savannah.gnu.org/releases/avr-libc/avr-libc-2.0.0.tar.bz2
 
-wget http://download.savannah.gnu.org/releases/avrdude/avrdude-6.1.tar.gz
+wget http://download.savannah.gnu.org/releases/avrdude/avrdude-6.3.tar.gz
 
-wget ftp://ftp.gnu.org/gnu/gdb/gdb-7.10.tar.xz
+wget ftp://ftp.gnu.org/gnu/gdb/gdb-7.11.tar.xz
 
 wget http://download.savannah.gnu.org/releases/simulavr/simulavr-1.0.0.tar.gz
